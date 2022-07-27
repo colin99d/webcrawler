f443e80d3dd99ed97efd2b87b01241159b3792a5
colin99d <colin99delahunty@gmail.com>
2022-07-27 12:04:39 -0400

    Fixed check_href

--- src/main.c
+++ src/main.c
@@ -19,7 +19,6 @@ char* substr(const char *src, int m, int n)
 			dest++;
 	}
 
-	// null-terminate the destination string
 	*dest = '\0';
 
 	return dest - len;
@@ -27,23 +26,12 @@ char* substr(const char *src, int m, int n)
 
 void check_href(char *html) {
 	int i;
-	int j;
 	char *sub_str;
-	char *tmp_str;
 
 	for (i = 0; i < strlen(html); i++) {
 		sub_str = substr(html, i, i+5);
 		if (strcmp(sub_str, "href=") == 0) {
-			i += 7;
-			j = i;
-			while (strcmp(&html[i], "\"") != 0) {
-				printf("%c", html[i]);
-				i++;
-			}
-			tmp_str = substr(html, j, i);
-
 			printf("%s\n", sub_str);
-			printf("%s\n", tmp_str);
 		}
 	}
 }
@@ -51,8 +39,7 @@ void check_href(char *html) {
 
 int main() {
 	struct MemoryStruct * html = get_url("https://www.example.com");
-  printf("%s\n", html->memory);
-	/* check_href(html->memory); */
+	check_href(html->memory);
 	free(html);
 	return 0;
 }
