291a23d1aeedff5660078246be90d6fee42017ae
colin99d <colin99delahunty@gmail.com>
2022-08-05 18:15:23 -0400

    Working on something

--- src/main.c
+++ src/main.c
@@ -16,14 +16,17 @@ TODO: take arguments for starting url
 
 static volatile int keepRunning = 1;
 
+typedef struct result {
+	char *address;
+	char *parent;
+} Result;
 
-int find_all(char *string, char *sub, char **result ) {
+int find_all(char *string, char *sub, Result *all_results) {
 	int i = 0;
 	char *sub_str;
-	char *found;
+	char found[2048];
 	int end;
 	int str_len;
-
 
 	str_len = strlen(string);
 	for(i = 0; i < str_len; i++) {
@@ -34,15 +37,15 @@ int find_all(char *string, char *sub, char **result ) {
 				if (end == -1) {
 					continue;
 				}
-				found = substr(string, i+6, end);
+				strcpy(found, substr(string, i+6, end));
 				sub_str = substr(string, i+6, i+10)	;
 				if(strcmp(sub_str, "http") == 0) {
 					if (strlen(found) > MAX_URL_LENGTH) {
 						continue;
 					}
-					realloc(result, sizeof(char *) * (i+1));
-					result[i] = malloc(sizeof(char) * (strlen(found) + 1));
-					strcpy(result[i], found);
+					all_results[i].address = malloc(sizeof(char) * (strlen(found) + 1));
+					strcpy(all_results[i].address, found);
+					purple(all_results[i].address);
 					i++;
 				}
 			}
@@ -51,14 +54,14 @@ int find_all(char *string, char *sub, char **result ) {
 	return i;
 }
 
-void add_item(sqlite3 *db, char *zErrMsg, URL *urls, int *total_urls, char *found) {
-	if (sql_duplicate(db, zErrMsg, found) == 1) {
-		red(found);
+void add_item(sqlite3 *db, char *zErrMsg, URL *urls, int *total_urls, Result the_result) {
+	if (sql_duplicate(db, zErrMsg, the_result.address) == 1) {
+		red(the_result.address);
 	} else {
-		yellow(found);
-		strcpy(urls[*total_urls].address, found);
+		yellow(the_result.address);
+		strcpy(urls[*total_urls].address, the_result.address);
 		urls[*total_urls].scanned = 0;
-		add_url(db, zErrMsg, found, 0);
+		add_url(db, zErrMsg, the_result.address, 0);
 		(*total_urls)++;
 	}
 }
@@ -68,21 +71,10 @@ void intHandler(int dummy) {
 	    printf("\nSaving...\n");
 }
 
-int main() {
-	MemoryStruct * html;
-	sqlite3 *db = 0;
-	URL *all_urls = 0;
-	char **find_results = 0;
-	int results_number;
+void getData(sqlite3 *db, char *zErrMsg, URL *all_urls, int *total_urls) {
 	int response;
-	char *zErrMsg = 0;
-	int total_urls = 0;
 	int i;
-
-	signal(SIGINT, intHandler);
-
 	response = sqlite3_open_v2("urls.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, "unix");
-
 	if(response) {
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		sqlite3_close_v2(db);
@@ -93,14 +85,34 @@ int main() {
 		sqlite3_free(zErrMsg);
 	}
 
-	total_urls = retrieve_urls(db, zErrMsg, all_urls);
-	printf("Total URLS at start: %i\n", total_urls);
+	(*total_urls) = retrieve_urls(db, zErrMsg, all_urls);
+	for (i = 0; i < *total_urls; i++) {
+		printf("%s\n", all_urls[i].address);
+	}
+	printf("Total URLS at start: %i\n", (*total_urls));
 	if(total_urls == 0) {
 		strcpy(all_urls[0].address, "https://www.example.com");
 		all_urls[0].scanned = 0;
 		add_url(db, zErrMsg, all_urls[0].address, 1);
 		total_urls++;
 	}
+	printf("-----1-----\n");
+
+}
+
+int main() {
+	MemoryStruct * html;
+	sqlite3 *db = 0;
+	URL *all_urls = malloc(sizeof(URL));
+	Result *all_results = malloc(sizeof(Result));
+	int results_number;
+	char *zErrMsg = 0;
+	int total_urls = 0;
+	int i;
+
+	signal(SIGINT, intHandler);
+	getData(db, zErrMsg, all_urls, &total_urls);
+
 	for(i = 0; i < total_urls; i++){
 		if (keepRunning == 0) {
 			break;
@@ -108,16 +120,23 @@ int main() {
 		if (all_urls[i].scanned == 0) {
 			cyan(all_urls[i].address);
 			html = get_url(all_urls[i].address);
-			results_number = find_all(html->memory, "href=", find_results);
+			results_number = find_all(html->memory, "href=", all_results);
+			printf("-----2-----\n");
 			for (i = 0; i < results_number; i++) {
-				add_item(db, zErrMsg, all_urls, &total_urls, find_results[i]);
+				green(all_results[i].address);
 			}
+			/*
+			for (i = 0; i < results_number; i++) {
+				add_item(db, zErrMsg, all_urls, &total_urls, all_results[i]);
+			}
+			printf("-----3-----\n");
 			all_urls[i].scanned = 1;
 			green(all_urls[i].address);
 			free(html);
 			printf("URLs found: %i\tURLs scanned: %i\n", total_urls, i);
+			*/
 		}
 	}
-	response = sqlite3_close_v2(db);
+	sqlite3_close_v2(db);
 	return 0;
 }
