e1df728f0b7c3c6fb58c1526100c29a5c5b94621
colin99d <colin99delahunty@gmail.com>
2022-08-05 21:25:25 -0400

    Debugging sqlite

--- src/_sqlite.c
+++ src/_sqlite.c
@@ -27,13 +27,16 @@ int sql_table_duplicate(sqlite3 *db, char *zErrMsg, char *table) {
 	response = sqlite3_prepare_v2(db, query, strlen(query), &statement, NULL);
 	if(response != SQLITE_OK) {
 		sqlite3_close(db);
-		printf("Can't retrieve data: %s\n", sqlite3_errmsg(db));
+		printf("SQL TABLE DUPLICATE: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(statement);
 		return -1;
 	}
 	while (sqlite3_step(statement) == SQLITE_ROW) {	
+		sqlite3_finalize(statement);
 		return 1;
 	}
 
+	sqlite3_finalize(statement);
 	return 0;
 }
 
@@ -67,6 +70,7 @@ int add_url(sqlite3 *db, char *zErrMsg, char *url, int first) {
 
 
 int sql_duplicate(sqlite3 *db, char *zErrMsg, char* str) {
+	printf("%s\n", str);
 	sqlite3_stmt * statement;
 	int response;
 	char query[2100];
@@ -74,12 +78,16 @@ int sql_duplicate(sqlite3 *db, char *zErrMsg, char* str) {
 	response = sqlite3_prepare_v2(db, query, strlen(query), &statement, NULL);
 	if(response != SQLITE_OK) {
 		sqlite3_close(db);
-		printf("Can't retrieve data: %s\n", sqlite3_errmsg(db));
+		printf("SQL DUPLICATE: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(statement);
+		exit(1);
 		return -1;
 	}
 	while (sqlite3_step(statement) == SQLITE_ROW) {	
+		sqlite3_finalize(statement);
 		return 1;	
 	}
+	sqlite3_finalize(statement);
 	return 0;
 }
 
@@ -101,7 +109,8 @@ int retrieve_urls(sqlite3 * db, char *zErrMsg, URL * purl) {
 	response = sqlite3_prepare_v2(db, query, strlen(query), &statement, NULL);
 	if(response != SQLITE_OK) {
 		sqlite3_close(db);
-		printf("Can't retrieve data: %s\n", sqlite3_errmsg(db));
+		printf("RETRIEVE URLS: %s\n", sqlite3_errmsg(db));
+		sqlite3_finalize(statement);
 		return -1;
 	}
 	while (sqlite3_step(statement) == SQLITE_ROW) {	
@@ -112,5 +121,22 @@ int retrieve_urls(sqlite3 * db, char *zErrMsg, URL * purl) {
 		purl[i].scanned = sqlite3_column_int(statement, 2);
 		i++;
 	}
+	sqlite3_finalize(statement);
 	return i;
 }
+/*
+int main(void) {
+	URL *all_urls = malloc(sizeof(URL));
+	sqlite3 *db = 0;
+	char *zErrMsg = 0;
+	int i;
+	
+	sqlite3_open_v2("urls.db", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, "unix");
+	create_table(db, zErrMsg);
+	retrieve_urls(db, zErrMsg, all_urls);
+	for(i=0; i<100000; i++) {
+		sql_duplicate(db, zErrMsg, "testsadwqdqecbqwhifbwqdnbqwidbiqwbdncqwejbceihwfghh2io");
+		printf("We checked\n");
+	}
+}
+*/
--- src/main.c
+++ src/main.c
@@ -23,6 +23,7 @@ typedef struct result {
 
 int find_all(char *string, char *sub, Result *all_results) {
 	int i = 0;
+	int j = 0;
 	char *sub_str;
 	char found[2048];
 	int end;
@@ -33,32 +34,41 @@ int find_all(char *string, char *sub, Result *all_results) {
 		if (str_len >= i + 10) {
 		sub_str = substr(string, i, i+5);
 			if(strcmp(sub_str, sub) == 0) {
+				printf("--------3.1--------\n");
 				end = find_next(string, '"', '\'', i+6);
 				if (end == -1) {
 					continue;
 				}
 				strcpy(found, substr(string, i+6, end));
+				printf("--------3.2--------\n");
 				sub_str = substr(string, i+6, i+10)	;
+				printf("%s\n", sub_str);
+				printf("--------3.3--------\n");
 				if(strcmp(sub_str, "http") == 0) {
 					if (strlen(found) > MAX_URL_LENGTH) {
 						continue;
 					}
-					all_results[i].address = malloc(sizeof(char) * (strlen(found) + 1));
-					strcpy(all_results[i].address, found);
-					purple(all_results[i].address);
-					i++;
+					printf("--------3.4--------\n");
+					all_results[j].address = malloc(sizeof(char) * (strlen(found) + 1));
+					printf("--------3.5--------\n");
+					strcpy(all_results[j].address, found);
+					printf("--------3.5--------\n");
+					j++;
 				}
 			}
 		}
 	}
-	return i;
+	return j;
 }
 
 void add_item(sqlite3 *db, char *zErrMsg, URL *urls, int *total_urls, Result the_result) {
-	if (sql_duplicate(db, zErrMsg, the_result.address) == 1) {
+	int response  = sql_duplicate(db, zErrMsg, the_result.address);
+	if (response == 1) {
 		red(the_result.address);
 	} else {
+		printf("Address size: %lu\n", sizeof(the_result.address));
 		yellow(the_result.address);
+		urls[*total_urls].address = malloc(sizeof(char) * (strlen(the_result.address) + 1));
 		strcpy(urls[*total_urls].address, the_result.address);
 		urls[*total_urls].scanned = 0;
 		add_url(db, zErrMsg, the_result.address, 0);
@@ -91,13 +101,12 @@ void getData(sqlite3 *db, char *zErrMsg, URL *all_urls, int *total_urls) {
 	}
 	printf("Total URLS at start: %i\n", (*total_urls));
 	if(total_urls == 0) {
+		all_urls[0].address = malloc(sizeof(char) * 25);
 		strcpy(all_urls[0].address, "https://www.example.com");
 		all_urls[0].scanned = 0;
 		add_url(db, zErrMsg, all_urls[0].address, 1);
 		total_urls++;
 	}
-	printf("-----1-----\n");
-
 }
 
 int main() {
@@ -107,7 +116,7 @@ int main() {
 	Result *all_results = malloc(sizeof(Result));
 	int results_number;
 	char *zErrMsg = 0;
-	int total_urls = 0;
+	int total_urls;
 	int i;
 
 	signal(SIGINT, intHandler);
@@ -118,23 +127,21 @@ int main() {
 			break;
 		}
 		if (all_urls[i].scanned == 0) {
+			printf("--------1--------\n");
 			cyan(all_urls[i].address);
+			printf("--------2--------\n");
 			html = get_url(all_urls[i].address);
+			printf("--------3--------\n");
 			results_number = find_all(html->memory, "href=", all_results);
-			printf("-----2-----\n");
-			for (i = 0; i < results_number; i++) {
-				printf("%s\n", all_results[i].address);
-			}
-			/*
+			printf("--------4--------\n");
 			for (i = 0; i < results_number; i++) {
 				add_item(db, zErrMsg, all_urls, &total_urls, all_results[i]);
 			}
-			printf("-----3-----\n");
+			printf("--------5--------\n");
 			all_urls[i].scanned = 1;
 			green(all_urls[i].address);
 			free(html);
 			printf("URLs found: %i\tURLs scanned: %i\n", total_urls, i);
-			*/
 		}
 	}
 	sqlite3_close_v2(db);
